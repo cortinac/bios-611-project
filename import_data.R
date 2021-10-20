b143871e1ee9815ef3b9ac2940d42a98c2f561a5
Christopher Cortina <cortinac@live.unc.edu>
2021-10-20 18:50:39 -0400

    Pull from API without using app token

--- import_data.R
+++ import_data.R
@@ -1,14 +1,11 @@
 # import_data.R
 # Import vaccination data from CDC API
 
-source("config.R")
+#source("config.R")
 
 library("RSocrata")
 library(tidyverse)
 
-system.time(vax_data <- read.socrata(
-  "https://data.cdc.gov/resource/8xkx-amqh.json",
-  app_token = token
-))
+system.time(vax_data <- read.socrata("https://data.cdc.gov/resource/8xkx-amqh.json"))
 
 write_csv(vax_data, "derived_data/vax_data.csv")
