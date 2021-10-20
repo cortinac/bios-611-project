eed3b4f511477b1c366f62c093ba75188c6a6938
Christopher Cortina <cortinac@live.unc.edu>
2021-10-20 00:22:47 -0400

    Add R file to import data from API.

---
+++ import_data.R
@@ -0,0 +1,14 @@
+# import_data.R
+# Import vaccination data from CDC API
+
+source("config.R")
+
+library("RSocrata")
+library(tidyverse)
+
+system.time(vax_data <- read.socrata(
+  "https://data.cdc.gov/resource/8xkx-amqh.json",
+  app_token = token
+))
+
+write_csv(vax_data, "derived_data/vax_data.csv")
