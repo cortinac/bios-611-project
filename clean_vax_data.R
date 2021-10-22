6c2cecfccc5b388bedefd6114891d94b4a05e037
Christopher Cortina <cortinac@live.unc.edu>
2021-10-22 19:39:34 -0400

    Update to work with new Docker instructions

--- clean_vax_data.R
+++ clean_vax_data.R
@@ -2,10 +2,10 @@
 # Clean the vaccination data
 
 library(tidyverse)
-vax_data <- read_csv("derived_data/vax_data.csv")
+vax_data <- read_csv("project/derived_data/vax_data.csv")
 
 clean <- vax_data %>%
   filter(fips!='UNK') %>%
   filter(series_complete_pop_pct > 0)
 
-write_csv(clean, "derived_data/clean.csv")
+write_csv(clean, "project/derived_data/clean.csv")
--- import_data.R
+++ import_data.R
@@ -8,4 +8,4 @@ library(tidyverse)
 
 system.time(vax_data <- read.socrata("https://data.cdc.gov/resource/8xkx-amqh.json?$where=date>='2021-08-01T00:00:00.000'"))
 
-write_csv(vax_data, "derived_data/vax_data.csv")
+write_csv(vax_data, "project/derived_data/vax_data.csv")
