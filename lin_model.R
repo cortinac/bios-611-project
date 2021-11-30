3443caba39fb8a841e02f643052afb03eb4c5e7a
Christopher Cortina <cortinac@live.unc.edu>
2021-11-30 01:02:10 -0500

    Simple linear regressions

---
+++ lin_model.R
@@ -0,0 +1,36 @@
+# lin_model.R
+# linear regression models for vax vs SVI data
+
+library(tidyverse)
+
+clean1115 <- read_csv("~/project/derived_data/clean1115.csv")
+
+lm1 <- glm(series_complete_pop_pct ~ RPL_THEME1 + RPL_THEME2 + RPL_THEME3 + RPL_THEME4 + EP_UNINSUR + metro_status, clean1115,
+           family=gaussian)
+summary(lm1)
+
+lm2 <- glm(chg_aug_1 ~ RPL_THEME1 + RPL_THEME2 + RPL_THEME3 + RPL_THEME4 + EP_UNINSUR + metro_status, clean1115,
+           family=gaussian)
+summary(lm2)
+
+lm3 <- glm(chg30 ~ RPL_THEME1 + RPL_THEME2 + RPL_THEME3 + RPL_THEME4 + EP_UNINSUR + metro_status, clean1115,
+           family=gaussian)
+summary(lm3)
+
+lm1_1 <- glm(series_complete_pop_pct ~ EP_POV + EP_UNEMP + EP_PCI + EP_NOHSDP + EP_AGE65 + EP_AGE17 +
+              EP_DISABL + EP_SNGPNT + EP_MINRTY + EP_LIMENG + EP_MUNIT + EP_MOBILE + EP_CROWD +
+              EP_NOVEH + EP_GROUPQ + EP_UNINSUR + metro_status, clean1115,
+             family=gaussian)
+summary(lm1_1)
+
+lm2_1 <- glm(chg_aug_1 ~ EP_POV + EP_UNEMP + EP_PCI + EP_NOHSDP + EP_AGE65 + EP_AGE17 +
+              EP_DISABL + EP_SNGPNT + EP_MINRTY + EP_LIMENG + EP_MUNIT + EP_MOBILE + EP_CROWD +
+              EP_NOVEH + EP_GROUPQ + EP_UNINSUR + metro_status, clean1115,
+             family=gaussian)
+summary(lm2_1)
+
+lm3_1 <- glm(chg30 ~ EP_POV + EP_UNEMP + EP_PCI + EP_NOHSDP + EP_AGE65 + EP_AGE17 +
+              EP_DISABL + EP_SNGPNT + EP_MINRTY + EP_LIMENG + EP_MUNIT + EP_MOBILE + EP_CROWD +
+              EP_NOVEH + EP_GROUPQ + EP_UNINSUR + metro_status, clean1115,
+             family=gaussian)
+summary(lm3_1)
