4074d1ae44a30643355786f4bc3123fe2a939443
Christopher Cortina <cortinac@live.unc.edu>
2021-12-05 16:19:00 -0500

    Cap date on data pull

--- import_data.R
+++ import_data.R
@@ -4,6 +4,6 @@
 library("RSocrata")
 library(tidyverse)
 
-system.time(vax_data <- read.socrata("https://data.cdc.gov/resource/8xkx-amqh.json?$where=date>='2021-08-01T00:00:00.000'"))
+system.time(vax_data <- read.socrata("https://data.cdc.gov/resource/8xkx-amqh.json?$where=date between '2021-08-01T00:00:00.000' and '2021-11-30T00:00:00.000'"))
 
 write_csv(vax_data, "~/project/derived_data/vax_data.csv")
