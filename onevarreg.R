1633dab01d1f6816830c8141f1beb8f9efa2b356
Christopher Cortina <cortinac@live.unc.edu>
2021-12-07 23:02:48 -0500

    Move extra files to folder

--- onevarreg.R
+++
@@ -1,9 +0,0 @@
-# onevarreg.R
-# Regression with just overall theme
-
-library(tidyverse)
-
-clean1115 <- read_csv("~/project/derived_data/clean1115.csv")
-
-lm1 <- lm(series_complete_pop_pct ~ RPL_THEMES, clean1115)
-summary(lm1)
