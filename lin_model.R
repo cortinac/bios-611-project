8603a191fc9e8b69377ecbc5a762ccc4e591b938
Christopher Cortina <cortinac@live.unc.edu>
2021-12-09 01:17:10 -0500

    Format linear model tables

--- lin_model.R
+++ lin_model.R
@@ -14,6 +14,11 @@ names(lm1$coefficients) <- c('(Intercept)', 'Theme 1: Socioeconomic Status',
                              'Theme 4: Housing Type & Transportation',
                              'Uninsured (%)', 'Metro Status (Reference = Non-metro)')
 summary(lm1)
+t1 <- coef(summary(lm1)) %>% as.data.frame() %>% mutate(Estimate=round(Estimate, digits=3),
+                                                        `Std. Error`=round(`Std. Error`, digits=3),
+                                                        `t value`=round(`t value`, digits=3),
+                                                        ` `=if_else(`Pr(>|t|)`<0.05, '*', ''),
+                                                        `Pr(>|t|)`=format.pval(`Pr(>|t|)`, digits=2, eps=0.001, nsmall=3))
 
 lm2 <- lm(chg_aug_1 ~ RPL_THEME1 + RPL_THEME2 + RPL_THEME3 + RPL_THEME4 + EP_UNINSUR + metro_status,
           clean2)
@@ -23,6 +28,11 @@ names(lm2$coefficients) <- c('(Intercept)', 'Theme 1: Socioeconomic Status',
                              'Theme 4: Housing Type & Transportation',
                              'Uninsured (%)', 'Metro Status (Reference = Non-metro)')
 summary(lm2)
+t2 <- coef(summary(lm2)) %>% as.data.frame() %>% mutate(Estimate=round(Estimate, digits=3),
+                                                        `Std. Error`=round(`Std. Error`, digits=3),
+                                                        `t value`=round(`t value`, digits=3),
+                                                        ` `=if_else(`Pr(>|t|)`<0.05, '*', ''),
+                                                        `Pr(>|t|)`=format.pval(`Pr(>|t|)`, digits=2, eps=0.001, nsmall=3))
 
 lm1_1 <- lm(series_complete_pop_pct ~ EP_POV + EP_UNEMP + EP_PCI + EP_NOHSDP + EP_AGE65 + EP_AGE17 +
               EP_DISABL + EP_SNGPNT + EP_MINRTY + EP_LIMENG + EP_MUNIT + EP_MOBILE + EP_CROWD +
@@ -34,6 +44,11 @@ names(lm1_1$coefficients) <- c('(Intercept)', 'Below Poverty (%)', 'Unemployed (
                                'Mobile Homes (%)', 'Crowding (%)', 'No Vehicle (%)', 'Group Quarters (%)',
                                'Uninsured (%)', 'Metro Status (Reference = Non-metro)')
 summary(lm1_1)
+t3 <- coef(summary(lm1_1)) %>% as.data.frame() %>% mutate(Estimate=round(Estimate, digits=3),
+                                                        `Std. Error`=round(`Std. Error`, digits=3),
+                                                        `t value`=round(`t value`, digits=3),
+                                                        ` `=if_else(`Pr(>|t|)`<0.05, '*', ''),
+                                                        `Pr(>|t|)`=format.pval(`Pr(>|t|)`, digits=2, eps=0.001, nsmall=3))
 
 lm2_1 <- lm(chg_aug_1 ~ EP_POV + EP_UNEMP + EP_PCI + EP_NOHSDP + EP_AGE65 + EP_AGE17 +
               EP_DISABL + EP_SNGPNT + EP_MINRTY + EP_LIMENG + EP_MUNIT + EP_MOBILE + EP_CROWD +
@@ -45,3 +60,8 @@ names(lm2_1$coefficients) <- c('(Intercept)', 'Below Poverty (%)', 'Unemployed (
                                'Mobile Homes (%)', 'Crowding (%)', 'No Vehicle (%)', 'Group Quarters (%)',
                                'Uninsured (%)', 'Metro Status (Reference = Non-metro)')
 summary(lm2_1)
+t4 <- coef(summary(lm2_1)) %>% as.data.frame() %>% mutate(Estimate=round(Estimate, digits=3),
+                                                        `Std. Error`=round(`Std. Error`, digits=3),
+                                                        `t value`=round(`t value`, digits=3),
+                                                        ` `=if_else(`Pr(>|t|)`<0.05, '*', ''),
+                                                        `Pr(>|t|)`=format.pval(`Pr(>|t|)`, digits=2, eps=0.001, nsmall=3))
