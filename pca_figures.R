62da3a02dd112368824f0c592faa2518a8f53433
Christopher Cortina <cortinac@live.unc.edu>
2021-12-08 23:43:36 -0500

    Adjust figure labels, scaling

--- pca_figures.R
+++ pca_figures.R
@@ -9,17 +9,17 @@ pca3 <- read_csv("~/project/derived_data/pca3.csv")
 pca4 <- read_csv("~/project/derived_data/pca4.csv")
 
 ggplot(pca1, aes(PC1, PC2)) + geom_point(aes(color=series_complete_pop_pct), alpha=0.5) +
-  scale_color_viridis_c()
-ggsave("~/project/figures/pca1_rate.png")
+  scale_color_viridis_c() + labs(color="Vaccination Rate")
+ggsave("~/project/figures/pca1_rate.png", width=9, height=6, units="in")
 
 ggplot(pca3, aes(PC1, PC2)) + geom_point(aes(color=chg_aug_1), alpha=0.5) +
-  scale_color_viridis_c()
-ggsave("~/project/figures/pca1_chg.png")
+  scale_color_viridis_c() + labs(color="Change from Aug 1")
+ggsave("~/project/figures/pca1_chg.png", width=9, height=6, units="in")
 
 ggplot(pca2, aes(PC1, PC2)) + geom_point(aes(color=series_complete_pop_pct), alpha=0.5) +
-  scale_color_viridis_c()
-ggsave("~/project/figures/pca2_rate.png")
+  scale_color_viridis_c() + labs(color="Vaccination Rate")
+ggsave("~/project/figures/pca2_rate.png", width=9, height=6, units="in")
 
 ggplot(pca4, aes(PC1, PC2)) + geom_point(aes(color=chg_aug_1), alpha=0.5) +
-  scale_color_viridis_c()
-ggsave("~/project/figures/pca2_chg.png")
+  scale_color_viridis_c() + labs(color="Change from Aug 1")
+ggsave("~/project/figures/pca2_chg.png", width=9, height=6, units="in")
--- themes_scatter.R
+++ themes_scatter.R
@@ -5,27 +5,29 @@ library(tidyverse)
 
 clean1115 <- read_csv("~/project/derived_data/clean1115.csv")
 
-ggplot(clean1115, aes(RPL_THEMES, series_complete_pop_pct)) + geom_point(aes(color=svi_ctgy), alpha=0.5)
-ggsave("~/project/figures/themes.png")
-ggplot(clean1115, aes(RPL_THEMES, chg_aug_1)) + geom_point(aes(color=svi_ctgy), alpha=0.5)
-ggsave("~/project/figures/themes_aug1.png")
+ggplot(clean1115, aes(RPL_THEMES, series_complete_pop_pct)) + geom_point(aes(color=svi_ctgy), alpha=0.5) +
+  xlab("Overall SVI Score") + ylab("COVID-19 Vaccination Rate") + scale_color_discrete("SVI Category")
+ggsave("~/project/figures/themes.png", width=9, height=6, units="in")
+ggplot(clean1115, aes(RPL_THEMES, chg_aug_1)) + geom_point(aes(color=svi_ctgy), alpha=0.5) +
+  xlab("Overall SVI Score") + ylab("Change in Vaccination Rate from August 1") + scale_color_discrete("SVI Category")
+ggsave("~/project/figures/themes_aug1.png", width=9, height=6, units="in")
 
 ggplot(clean1115, aes(RPL_THEME1, series_complete_pop_pct)) + geom_point(aes(color=svi_ctgy), alpha=0.5)
-ggsave("~/project/figures/theme1.png")
+ggsave("~/project/figures/theme1.png", width=9, height=6, units="in")
 ggplot(clean1115, aes(RPL_THEME1, chg_aug_1)) + geom_point(aes(color=svi_ctgy), alpha=0.5)
-ggsave("~/project/figures/theme1_aug1.png")
+ggsave("~/project/figures/theme1_aug1.png", width=9, height=6, units="in")
 
 ggplot(clean1115, aes(RPL_THEME2, series_complete_pop_pct)) + geom_point(aes(color=svi_ctgy), alpha=0.5)
-ggsave("~/project/figures/theme2.png")
+ggsave("~/project/figures/theme2.png", width=9, height=6, units="in")
 ggplot(clean1115, aes(RPL_THEME2, chg_aug_1)) + geom_point(aes(color=svi_ctgy), alpha=0.5)
-ggsave("~/project/figures/theme2_aug1.png")
+ggsave("~/project/figures/theme2_aug1.png", width=9, height=6, units="in")
 
 ggplot(clean1115, aes(RPL_THEME3, series_complete_pop_pct)) + geom_point(aes(color=svi_ctgy), alpha=0.5)
-ggsave("~/project/figures/theme3.png")
+ggsave("~/project/figures/theme3.png", width=9, height=6, units="in")
 ggplot(clean1115, aes(RPL_THEME3, chg_aug_1)) + geom_point(aes(color=svi_ctgy), alpha=0.5)
-ggsave("~/project/figures/theme3_aug1.png")
+ggsave("~/project/figures/theme3_aug1.png", width=9, height=6, units="in")
 
 ggplot(clean1115, aes(RPL_THEME4, series_complete_pop_pct)) + geom_point(aes(color=svi_ctgy), alpha=0.5)
-ggsave("~/project/figures/theme4.png")
+ggsave("~/project/figures/theme4.png", width=9, height=6, units="in")
 ggplot(clean1115, aes(RPL_THEME4, chg_aug_1)) + geom_point(aes(color=svi_ctgy), alpha=0.5)
-ggsave("~/project/figures/theme4_aug1.png")
+ggsave("~/project/figures/theme4_aug1.png", width=9, height=6, units="in")
